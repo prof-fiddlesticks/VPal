71781398d5867b2ff2e10954cf1f80f216f5945f
prof-fiddlesticks <ishaandokania1@gmail.com>
2026-01-11 22:22:09 -0800

    Initialize VPal with basic Pygame setup

---
+++ VPal.py
@@ -0,0 +1,48 @@
+import pygame
+import sys
+
+#PATH -- C:\Users\ishaa\OneDrive\Desktop\VirtualPal\VPal.py
+
+pygame.init()
+
+HEIGHT = 600
+WIDTH = 900
+
+screen = pygame.display.set_mode((WIDTH, HEIGHT))
+pygame.display.set_caption("VPal")
+
+clock = pygame.time.Clock()
+font = pygame.font.SysFont(None, 48)
+
+running = True
+while running:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            running = False
+    
+    screen.fill((30,30,30))
+
+    def draw_bar(label, x, y, width, height, percent, fill_color):
+
+        bg = pygame.Rect(x, y , width, height)
+        pygame.draw.rect(screen, (143, 150, 143), bg)
+
+        fill_width = int(width * percent)
+        fill = pygame.Rect(x, y, width, height)
+        pygame.draw.rect(screen, (fill_color), fill)
+
+        text = font.render(label, True, (255, 255, 255))
+        screen.blit(text, (x, y - 40))
+     
+
+    draw_bar("Hunger", 600, 100, 200, 30, 0.3, (222, 152, 53))
+    draw_bar("Happy",  600, 220, 200, 30, 0.8, (230, 36, 18))
+
+    pygame.display.flip()
+    clock.tick(60)
+
+pygame.quit()
+sys.exit()
+
+
+ 
