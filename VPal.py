340aec2860d42220aca4ef6a35e7ba15c17d9948
prof-fiddlesticks <ishaandokania1@gmail.com>
2026-01-13 14:46:01 -0800

    Update VPal.py

--- VPal.py
+++ VPal.py
@@ -1,7 +1,7 @@
 import pygame
 import sys
 
-#RUN THIS CODE WITH PYTHON 3.13 IF YOU GET A WHEEL ERROR IN PYTHON 3.14
+#RUN WITH py -3.13 C:\Users\ishaa\Desktop\VirtualPet\VPal.py
 
 pygame.init()
 
@@ -36,8 +36,9 @@ while running:
      
 
     draw_bar("Hunger", 600, 100, 200, 30, 0.3, (222, 152, 53))
-    draw_bar("Happy",  600, 220, 200, 30, 0.8, (230, 36, 18))
-    draw_bar("Energy", 600, 340, 200, 30, 0.6, (11, 222, 187))
+    draw_bar("Thirst", 600, 220, 200, 30, 0.4, (52, 121, 217))
+    draw_bar("Happy",  600, 340, 200, 30, 0.8, (230, 36, 18))
+    draw_bar("Energy", 600, 460, 200, 30, 0.6, (11, 222, 187))
 
 
     pygame.display.flip()
